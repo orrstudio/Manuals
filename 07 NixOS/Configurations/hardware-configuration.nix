a905d7353267b2e115d563181f1453cb7b431093
Oruj Gafarov <65281943+orrstudio@users.noreply.github.com>
2024-03-24 01:05:26 +0400

    Update hardware-configuration.nix

--- 07 NixOS/Configurations/hardware-configuration.nix
+++ 07 NixOS/Configurations/hardware-configuration.nix
@@ -50,6 +50,6 @@
   # networking.interfaces.ens5.useDHCP = lib.mkDefault true;
   # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
 
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
